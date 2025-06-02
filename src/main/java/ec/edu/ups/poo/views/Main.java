4bb892ea1a2f9a1b74647ccfc13284b9f2731a0c
Diana Borja <valeriaborja975@gmail.com>
2025-06-01 20:43:51 -0500

    fix: Ventana de Registro de Calificaciones

---
+++ src/main/java/ec/edu/ups/poo/views/Main.java
@@ -0,0 +1,7 @@
+package ec.edu.ups.poo.views;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaRegistro ventana = new VentanaRegistro();
+    }
+}
---
+++ src/main/java/ec/edu/ups/poo/views/VentanaRegistro.java
@@ -0,0 +1,76 @@
+package ec.edu.ups.poo.views;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VentanaRegistro extends Frame implements ActionListener {
+
+    private Panel panelSuperior;
+    private Button botonAgregar;
+    private Button botonModificar;
+    private Button botonEliminar;
+
+    private Panel panelCentral;
+    private Label labelCedula;
+    private Label labelNombre;
+    private Label labelApellido;
+    private TextField txtCedula;
+    private TextField txtNombre;
+    private TextField txtApellido;
+
+    private Panel panelInferior;
+    private Button botonGuardar;
+    private Button botonCancelar;
+
+    public VentanaRegistro() {
+        setTitle("Registro de Calificaciones");
+        setSize(400, 200);
+        setBackground(Color.WHITE);
+        setLayout(new BorderLayout());
+        setLocationRelativeTo(null);
+
+
+        panelSuperior = new Panel(new FlowLayout());
+        botonAgregar = new Button("Agregar");
+        botonModificar = new Button("Modificar");
+        botonEliminar = new Button("Eliminar");
+        panelSuperior.add(botonAgregar);
+        panelSuperior.add(botonModificar);
+        panelSuperior.add(botonEliminar);
+        add(panelSuperior, BorderLayout.NORTH);
+
+
+        panelCentral = new Panel(new GridLayout(3, 2, 10, 10));
+        labelCedula = new Label("Cédula:");
+        txtCedula = new TextField();
+        labelNombre = new Label("Nombre:");
+        txtNombre = new TextField();
+        labelApellido = new Label("Apellido:");
+        txtApellido = new TextField();
+        panelCentral.add(labelCedula);
+        panelCentral.add(txtCedula);
+        panelCentral.add(labelNombre);
+        panelCentral.add(txtNombre);
+        panelCentral.add(labelApellido);
+        panelCentral.add(txtApellido);
+        add(panelCentral, BorderLayout.CENTER);
+
+        panelInferior = new Panel(new FlowLayout(FlowLayout.CENTER));
+        botonGuardar = new Button("Guardar");
+        botonCancelar = new Button("Cancelar");
+        panelInferior.add(botonGuardar);
+        panelInferior.add(botonCancelar);
+        add(panelInferior, BorderLayout.SOUTH);
+
+        setVisible(true);
+        botonGuardar.addActionListener(this);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("--Datos ingresados--");
+        System.out.println("Cédula: " + txtCedula.getText());
+        System.out.println("Nombre: " + txtNombre.getText());
+        System.out.println("Apellido: " + txtApellido.getText());
+    }
+}
